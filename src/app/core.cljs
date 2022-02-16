2829221d5e1afa588053dbf411a2d1b028895eac
Kovas Palunas <kovas.palunas@gmail.com>
2022-02-16 13:18:20 -0800

    working run generation

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -3,7 +3,8 @@
    [spec-tools.data-spec :as ds]
    [clojure.test.check.generators]
    [ghostwheel.core :as g :refer [>defn >defn- >fdef => | <- ?]]
-   [cljs.spec.gen.alpha :as gen]
+   ; [cljs.spec.gen.alpha :as gen]
+   [clojure.test.check.generators :as gen]
    [cljs.spec.alpha :as s]
    [reagent.core :as r]
    [clojure.set :refer [union]]
@@ -17,9 +18,10 @@
 (s/def ::metric-key
   (s/with-gen
     (s/and string?
-           #(< (count %) 3))
-    (fn [] (gen/fmap #(subs % 0 2)
-                     (gen/string-alphanumeric)))))
+           #(< 0 (count %) 3))
+    (fn [] (gen/such-that #(not= % "")
+             (gen/fmap #(subs % 0 2)
+                       gen/string-alphanumeric)))))
 
 (def sample
   "A single sample containing values for zero or more metrics."
@@ -39,26 +41,31 @@
 
 (def run
   "A single sample containing values for one or more metrics."
-  (ds/spec ::run
-    {:key ::key
-     :samples (s/with-gen
-                (s/and (s/coll-of ::sample)
-                       (fn [samples]
-                         (= 1 (count (get-unique-metric-keys samples))))) 
-                (fn [] 
-                  (let [num-metrics (gen/generate gen/small-integer)
-                        metric-keys (s/gen (gen/sample (gen/set ::metric-key)
-                                                       num-metrics))]
-                    (gen/vector (gen/hash-map
-                                  {:key (s/gen ::key)
-                                   :timestamp (s/gen ::timestamp)
-                                   :values (gen/vector-distinct
-                                             (gen/hash-map
-                                               :metric-key metric-keys
-                                               :value (s/gen float?))
-                                             num-metrics)})))))}))
+  (let [num-gen-metrics (Math/abs (gen/generate gen/small-integer))]
+    (ds/spec ::run
+      {:key ::key
+       :samples (s/with-gen
+                  (s/and (s/coll-of ::sample)
+                         (fn [samples]
+                           (= num-gen-metrics
+                              (count (get-unique-metric-keys samples))))) 
+                  #(let [metric-keys (gen/generate
+                                       (gen/set (s/gen ::metric-key)
+                                                {:num-elements
+                                                 num-gen-metrics}))
+                         values-gen (apply gen/tuple
+                                           (repeat (count metric-keys)
+                                                   (s/gen float?)))]
+                     (gen/vector
+                       (gen/hash-map
+                         :key (s/gen ::key)
+                         :timestamp (s/gen ::timestamp)
+                         :values (gen/fmap (fn [vals]
+                                            (mapv (fn [k v]
+                                                    {:metric-key k :value v})
+                                                  metric-keys vals))
+                                  values-gen)))))})))
 (s/def ::run run)
-
 
 (>defn get-num-metrics
   "Returns the number of metrics in the given run."
@@ -75,9 +82,9 @@
   ;   count))
 
 
-(def gened-run (gen/generate (s/gen ::run)))
+(gen/generate (s/gen ::run))
 
-(get-num-metrics gened-run)
+; (get-num-metrics gened-run)
   
 
 
