483b5fd379be1e2ab6281143ee4b73533fc404fd
Kovas Palunas <kovas.palunas@gmail.com>
2025-08-15 22:56:08 -0700

    experimentation

--- src/website_helpers/global.cljs
+++ src/website_helpers/global.cljs
@@ -6,13 +6,16 @@
     [website-helpers.utils :refer [get-url-param-selections]]))
 
 (def show-unselected-nodes-in-graph? (r/atom true))
+; whenever this is updated, the page graph will be redrawn
 (def graph-update-num (r/atom 0))
 (def url-params (r/atom {}))
 (def notes (r/atom website-helpers.test-data/notes))
 
 (defn get-all-note-categories
   [notes]
-  (reduce union (map :categories notes)))
+  (doto
+    (reduce union (map :categories notes))
+    prn))
 
 ; Map of string to boolean
 (def category-selections
@@ -20,12 +23,17 @@
 
 (defn sync-category-selections!
   []
+  ; manually for the graph to update when new categories are selected.
   (swap! graph-update-num inc)
   (reset! category-selections (get-url-param-selections
                                 (get-all-note-categories @notes)
                                 url-params)))
 
-(sync-category-selections!)
+; just do the category selection without redrawing the graph on first page load
+; in hopes of better performance
+(reset! category-selections (get-url-param-selections
+                              (get-all-note-categories @notes)
+                              url-params))
 
 (defn ^:export set-global-notes
   [new-notes]
